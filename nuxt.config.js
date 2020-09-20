ec360406277b4a68d8571e17217e783138eb69a8
sharan <sharnkmp@gmail.com>
2020-09-20 15:46:10 +0530

    added router to target

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,9 @@ export default {
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: 'static',
+  router:{
+    base: '/loosearticles.github.io/'
+  },
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
