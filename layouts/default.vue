857eb6863eb9e08fe5308b217e7dd2f2ab6df470
Naveen Naidu <mnaveennaidu9927@gmail.com>
2020-12-12 18:31:53 +0900

    added color mode switch

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,17 @@
 <template>
   <div>
-    <div class="py-12 ">
-      <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 ">
+    <div class="py-12">
+      <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+        <ColorModeSwitch />
         <div class="lg:text-center pb-10">
-          <p class="p-4 pb-2 text-right rounded-lg">
-            <select
-              v-model="$colorMode.preference"
-              class="border w-28 h-8 dark:bg-gray-900 dark:border-gray-700"
-            >
-              <option value="system">System Theme</option>
-              <option value="light">Light</option>
-              <option value="dark">Dark</option>
-            </select>
-          </p>
-          <p 
+          <p
             class="text-base text-center leading-6 text-indigo-600 font-semibold tracking-wide uppercase"
           >
             Sharan Pujari
           </p>
           <nuxt-link to="/">
             <h3
-              class="mt-2  text-center text-4xl leading-8 font-extrabold tracking-tight sm:text-4xl sm:leading-10"
+              class="mt-2 text-center text-4xl leading-8 font-extrabold tracking-tight sm:text-4xl sm:leading-10"
             >
               Travel Bloticles
             </h3>
@@ -33,7 +24,6 @@
 </template>
 
 <style>
-
 /*
 To enable sacramento font, uncomment one of these: @import or @font-face
 Then add name in class of <p> above
@@ -53,7 +43,6 @@ p.name {
 }
 */
 
-
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -64,9 +53,7 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
-  
 }
-
 
 *,
 *::before,
@@ -103,8 +90,4 @@ html {
   color: #fff;
   background-color: #35495e;
 }
-
-
-
-
 </style>
